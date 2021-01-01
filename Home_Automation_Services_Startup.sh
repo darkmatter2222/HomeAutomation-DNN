cba9c2fd8aedfecb3b129f14f1942ac2ca64a951
ryan susman <ryansusman@gmail.com>
2021-01-01 10:17:57 -0600

    move to Single Operator

--- Home_Automation_Services_Startup.sh
+++ Home_Automation_Services_Startup.sh
@@ -1,4 +1,3 @@
 cd /home/pi/HomeAutomation-DNN
 /usr/bin/python3 Services/PubSub_Nest_Subscriber.py &
-/usr/bin/python3 Services/OAuth_Token_Refresh_Engine.py &
-/usr/bin/python3 Services/Device_Metadata_Updater.py
+/usr/bin/python3 Services/OAuth_Token_Refresh_Engine.py

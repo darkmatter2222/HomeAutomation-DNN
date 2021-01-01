f18e9b1216eb06bae87942a450cec5ba3339dbc0
ryan susman <ryansusman@gmail.com>
2021-01-01 10:16:43 -0600

    move to Single Operator

--- Services/PubSub_Nest_Subscriber.py
+++ Services/PubSub_Nest_Subscriber.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import json, os, subprocess, datetime
 import uuid
 from pymongo import MongoClient
+from datetime import datetime, timezone, timedelta
 from google.oauth2 import service_account
 from google.cloud import pubsub_v1
 from bson.objectid import ObjectId
@@ -31,6 +32,10 @@ else:
 env_path = Path(target_env_path) / '.env'
 load_dotenv(dotenv_path=env_path)
 
+nest_metadata = {'devices': {}}
+nest_metadata_expiration_seconds = 3600
+nest_metadata_last_refresh = datetime.now(timezone.utc) - timedelta(minutes=60)
+
 my_logger.info('Connecting to MongoDB')
 my_client = MongoClient('mongodb://susmanserver:27017',
                         username=os.getenv("Google_Nest_Username"),
@@ -53,7 +58,7 @@ def getaccesstoken():
     token = my_col.find({'_id': ObjectId('5fd61e859532201850007cdf')})[0]['access_token']
     return token
 
-def pullimage(device_id, event_id, access_token):
+def pullimage(device_id, event_id, access_token, payload):
     url = f'https://smartdevicemanagement.googleapis.com/v1/{device_id}:executeCommand'
     headers = {'Content-Type': 'application/json', 'Authorization': f'Bearer {access_token}'}
     data = {"command": "sdm.devices.commands.CameraEventImage.GenerateImage", "params": {"eventId": event_id}}
@@ -65,7 +70,14 @@ def pullimage(device_id, event_id, access_token):
     response = requests.get(url=url, headers=headers)
     image = Image.open(io.BytesIO(response.content))
     unique_id = str(uuid.uuid1())
-    image.save(f"{target_image_root}/{unique_id}.jpeg")
+
+    this_image_target = \
+        f"{target_image_root}\{payload['resourceUpdate']['displayName']}\{payload['resourceUpdate']['type']}"
+
+    if not os.path.exists(this_image_target):
+        os.makedirs(this_image_target)
+
+    image.save(f"{this_image_target}/{unique_id}.jpeg")
     return unique_id
 
 def pullimages(payload):
@@ -76,12 +88,57 @@ def pullimages(payload):
                     access_token = getaccesstoken()
                     image_uid = pullimage(device_id=payload['resourceUpdate']['name'],
                               event_id=payload['resourceUpdate']['events'][key]['eventId'],
-                              access_token=access_token)
+                              access_token=access_token,
+                              payload=payload)
                     payload['resourceUpdate']['events'][key]['image_uid'] = image_uid
 
     except Exception as e:
         my_logger.critical(e)
 
+    return payload
+
+def pullmetainfo(last_refresh):
+    perform_refresh = False
+    if last_refresh < datetime.now(timezone.utc):
+        last_refresh = datetime.now(timezone.utc) + timedelta(minutes=nest_metadata_expiration_seconds)
+        perform_refresh = True
+    if perform_refresh:
+        try:
+            my_logger.info('Connecting to MongoDB, pulling auth')
+            db_name = 'OAuth2_Manager'
+            collection_name = 'Active_OAuth2'
+            my_client = MongoClient('mongodb://susmanserver:27017',
+                                    username=os.getenv("OAuth2_Manager_Username"),
+                                    password=os.getenv("OAuth2_Manager_Password"),
+                                    authSource=db_name,
+                                    authMechanism='SCRAM-SHA-256')
+            my_db = my_client[db_name]
+            my_col = my_db[collection_name]
+            token = my_col.find({'_id': ObjectId('5fd61e859532201850007cdf')})[0]
+            my_logger.info('Retrieving Devices')
+            url = f'https://smartdevicemanagement.googleapis.com/v1/enterprises/{os.getenv("Google_Nest_Prodect_ID")}/devices'
+            headers = {'Content-Type': 'application/json', 'Authorization': f'Bearer {token["access_token"]}'}
+            response = requests.get(url=url, headers=headers)
+            response_json = json.loads(response.text)
+            for device in response_json['devices']:
+                nest_metadata['devices'][device['name']] = \
+                    {
+                        'type': device['type'].replace('.', '_'),
+                        'displayName': device['parentRelations'][0]['displayName']
+                    }
+
+            my_logger.info(f'Meta Downloaded')
+        except Exception as e:
+            my_logger.critical(e)
+    return last_refresh
+
+def applymetadata(payload):
+    try:
+        payload['resourceUpdate']['type'] = nest_metadata['devices'][payload['resourceUpdate']['name']]['type']
+        payload['resourceUpdate']['displayName'] = \
+            nest_metadata['devices'][payload['resourceUpdate']['name']]['displayName']
+    except Exception as e:
+        my_logger.critical(e)
     return payload
 
 def scrubpayload(payload):
@@ -113,6 +170,7 @@ def scrubpayload(payload):
 
 
 def callback(message):
+    global nest_metadata_last_refresh
     try:
         my_logger.debug(message.data.decode('UTF-8'))
     except Exception as e:
@@ -120,10 +178,12 @@ def callback(message):
     my_logger.debug('Starting Callback')
     try:
         payload = message.data.decode('UTF-8')
-        json_payload = json.loads(payload)
-        scrubbed_json_payload = scrubpayload(json_payload)
-        pulled_scrubbed_json_payload = pullimages(scrubbed_json_payload)
-        insert_id = my_col.insert_one(pulled_scrubbed_json_payload).inserted_id
+        json_payload_1 = json.loads(payload)
+        json_payload_2 = scrubpayload(json_payload_1)
+        nest_metadata_last_refresh = pullmetainfo(nest_metadata_last_refresh)
+        json_payload_3 = applymetadata(json_payload_2)
+        json_payload_4 = pullimages(json_payload_3)
+        insert_id = my_col.insert_one(json_payload_4).inserted_id
         my_logger.debug(insert_id)
     except Exception as e:
         my_logger.critical(e)

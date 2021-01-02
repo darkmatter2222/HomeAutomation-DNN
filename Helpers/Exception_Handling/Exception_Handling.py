39344a6b0fbd7fef39624e9a9a6f69c4c8f7eec4
ryan susman <ryansusman@gmail.com>
2021-01-02 11:19:01 -0600

    Testing

--- Helpers/Exception_Handling/Exception_Handling.py
+++ Helpers/Exception_Handling/Exception_Handling.py
@@ -4,3 +4,6 @@ import traceback
 def formatexception(exception):
     print(traceback.format_exc())
 
+def test(exception):
+    print(exception)
+
--- Testing/HelperTester.py
+++ Testing/HelperTester.py
@@ -1,9 +1,25 @@
 import Helpers.Exception_Handling.Exception_Handling as eh
 
+import logging.handlers
+import sys, os
+
+my_logger = logging.getLogger('MyLogger')
+my_logger.setLevel(logging.INFO)
+if os.name != 'nt':
+    handler = logging.handlers.SysLogHandler(address='/dev/log')
+    my_logger.addHandler(handler)
+
+my_logger.info('Initializing .env')
+target_image_root = '/home/pi/neural/HomeAutomation-DNN/images'
+target_env_path = '/home/pi/secure'  # default linux
+if os.name == 'nt':
+    target_env_path = '\\\\SUSMANSERVER\\Active Server Drive\\HomeAutomation' # Windows
+    target_image_root = '../../testing'
+else:
+    sys.path.append('/home/pi/HomeAutomation-DNN')  # add this to the path
+    sys.path.append('/home/pi/secure')  # add this to the path
+    sys.path.append('/home/pi/Helpers')  # add this to the path
+    sys.path.append('/home/pi/Helpers/Exception_Handling')  # add this to the path
 
 
-try:
-    lol = 0
-    lol = 1 / lol
-except Exception as e:
-    eh.formatexception(exception=e)
+eh.test('If you are seeing this, it worked')

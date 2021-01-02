e2d50068f520a6c2c5abaf869960cc5d783a3db0
ryan susman <ryansusman@gmail.com>
2021-01-02 11:50:15 -0600

    Improve Logging

--- Home_Automation_Services_Startup.sh
+++ Home_Automation_Services_Startup.sh
@@ -1,3 +1,3 @@
 cd /home/pi/HomeAutomation-DNN
 /usr/bin/python3 Services/PubSub_Nest_Subscriber.py &
-/usr/bin/python3 Services/OAuth_Token_Refresh_Engine.py *
+/usr/bin/python3 Services/OAuth_Token_Refresh_Engine.py

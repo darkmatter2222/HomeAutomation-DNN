e3a160cb6643c06b8524a1944e04f9621917654b
ryan susman <ryansusman@gmail.com>
2021-01-02 11:49:34 -0600

    Improve Logging

--- Home_Automation_Services_Startup.sh
+++ Home_Automation_Services_Startup.sh
@@ -1,4 +1,3 @@
 cd /home/pi/HomeAutomation-DNN
 /usr/bin/python3 Services/PubSub_Nest_Subscriber.py &
 /usr/bin/python3 Services/OAuth_Token_Refresh_Engine.py *
-sudo journalctl -u pubsub.service -f -n 20
--- Home_Automation_Services_Update.sh
+++ Home_Automation_Services_Update.sh
@@ -3,3 +3,4 @@ cd /home/pi/HomeAutomation-DNN
 git reset --hard
 git pull
 sudo systemctl start pubsub.service
+sudo journalctl -u pubsub.service -f -n 20
